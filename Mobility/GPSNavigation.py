a779a7ff65003084347cfdb3ba8bbfef18a1f72a
noah <Noah.t.over-1@ou.edu>
2017-11-11 00:51:31 -0600

    added GPS comms and reorgonised
    I Know it looks like I changed a lot, but mostly just reorgonised. I also added a parser to read the coordinates from the console.

--- Mobility/GPSNavigation.py
+++ Mobility/GPSNavigation.py
@@ -1,5 +1,9 @@
 import math
-# import GPSComms as gps_c
+# things for GPS comunication
+# make sure to run pip install sbp
+from sbp.client.drivers.network_drivers import TCPDriver
+from sbp.client import Handler, Framer
+from sbp.navigation import SBP_MSG_POS_LLH, MsgPosLLH
 
 """ This code provides the rover with autonomous GPS navigation capabilites.
     There will be two ways of inputting the destination coordinates:
@@ -8,20 +12,62 @@ import math
         - Read Lat/Lon decimal values from a file. (Will remove human error (double/triple check values before running the code)
     This code then takes the Lat/Lon values and uses the Haversine formula to calculate distance to destination and also calculates
         Theta, which is the closest angle to the destination, offset from North. (e.g. +/-180 degrees from North)"""
+#TODO: move some variables to global variables to improve readability
 
 def main():
+    args = getArgs()
+    driver = TCPDriver('192.168.1.222', '55555')
+    #Location of rover, from the top.
+    #driver.read is the literal output from the tcp driver
+    #framer turns bytes into SBP messages (swift binary protocol)
+    #handler is an itterable to read the messages
+    with Handler(Framer(driver.read, None, verbose=True)) as source:
+        #reeds all the messages in a loop as they are recieved
+        for msg, metadata in source.filter(SBP_MSG_POS_LLH):
+            #Shouldnt We need an elivation?.. that's msg.height
+            #int of acuracy is [h/v]_acuracy also there is n_sats
+            roverLat = math.radians(msg.lat)
+            roverLong = math.radians(msg.lon)
+
+            #Get longitude, latitude, (not yet...height, and number of satellites) - need it in degrees
+            longitude = math.radians(args.longitude[0])
+            latitude = math.radians(args.latitude[0])
+
+            distance = getDistance(longitude, latitude, roverLat, roverLong)
+            theta = getBearing(longitude, latitude, roverLat, roverLong)
+
+            #TODO: turn(bearing) # somthing like: if math.abs(bearing) > tolerence then turn else drive(distance)
+            #TODO: drive(distance)
+
+            #Need to fix this to output the proper value of theta
+            print("Bearing: ", 360 + math.degrees(theta), ", Distance: ", d)
+
+            # If we're within 2 meters of the destination
+            if (distance <= 2):
+                print("Distance is within 2 meters of the destination.")
+                #TODO: put a break to say we are finnished
+def getArgs():
+    """
+    Get and parse arguments.
+    """
+    import argparse
+    parser = argparse.ArgumentParser(
+        description="Swift Navigation SBP Example.")
+    parser.add_argument(
+        "--longitude",
+        default=0,
+        nargs=1,
+        help="specify the longitude in decimal.")
+    parser.add_argument(
+        "--latitude",
+        default=0,
+        nargs=1,
+        help="specify the latitude in decimal.")
+    return parser.parse_args()
+
+def getDistance(longitude, latitude, roverLat, roverLong):
     # Radius of the Earth
-    RADIUS_OF_EARTH = 6371 * (1000);
-
-    #Location of rover. 0s are placeholders - need it in degrees
-    # roverLat = math.radians(getLat())
-    # roverLong = math.radians(getLon())
-
-    #Get longitude, latitude, height, and number of satellites - need it in degrees
-    #0s are placeholders
-    longitude = math.radians(0)
-    latitude = math.radians(0)
-
+    RADIUS_OF_EARTH = 6371 * (1000)
     # Haversine Formula: a = sin((lat2-lat1)/2) * sin((lat2-lat1)/2) + cos(lat1) * cos(lat2) * sin((lon2-lon1)/2) * sin((lon2-lon1)/2)
     # Calulate change in Latitude
     deltaLat = latitude - roverLat
@@ -31,40 +77,34 @@ def main():
     a = math.sin(deltaLat/2) * math.sin(deltaLat/2) + math.cos(roverLat) * math.cos(latitude) * math.sin(deltaLon/2) * math.sin(deltaLon/2)
     c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
     d = RADIUS_OF_EARTH * c
+    return d
 
+def getBearing(longitude, latitude, roverLat, roverLong):
+    # Calulate change in Latitude
+    deltaLat = latitude - roverLat
+    # Calculate change in Longitude
+    deltaLon = longitude - roverLong
     # Bearing, represented as nearest offset from North. (+/-180 degrees from North)
     theta = math.atan2(math.sin(deltaLon) * math.cos(latitude), math.cos(roverLat) * math.sin(latitude) - math.sin(roverLat) * math.cos(latitude) * math.cos(deltaLon))
+    return theta
 
-    # Make a turning method
+def drive(distance):
+    #TODO: Make a drive method
+    return 0
+def turn(bearing):
+    #TODO: Make a turning method
     """if (bearingDiff >= 90):
         turnInPlace(bearingDiff)
     else:
         turnInArc(bearingDiff)"""
-    #turn(bearing)
-        
-    # Make a drive method
-    #drive(distance)
+    return 0
+def turnInPlace(bearing):
+    #TODO: finnish this
+    return 0
+def turnInArc(bearing):
+    #TODO: finnish this
+    return 0
 
-    #Need to fix this to output the proper value of theta
-    print("Bearing: ", 360 + math.degrees(theta), ", Distance: ", d)
-
-    # If we're within 2 meters of the destination
-    if (distance <= 2):
-        print("Distance is within 2 meters of the destination.")
 
 if(__name__=="__main__"):
     main()
-
-def getLat():
-    # return gps_c.getLat()
-def getLon():
-    # return gps_c.getLon()
-def getBearing():
-    # return gps_c.getBearing()
-    
-"""def turnInPlace(bearing):
-    return 0;
-
-def turnInArc(bearing):
-    return 0;"""
-
