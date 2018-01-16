dbd42da62416e686592c16d2dce71c92db6f5269
NoahIsCool <noah.t.over-1@ou.edu>
2018-01-15 18:15:21 -0600

    Delete OldArmControll Test.py

--- testing/OldArmControll Test.py
+++
@@ -1,6 +0,0 @@
-from inputs import get_gamepad
-while 1:
-     events = get_gamepad()
-     for event in events:
-         if event.code is 'ABS_Y':
-             print(event.ev_type, event.code, event.state)
