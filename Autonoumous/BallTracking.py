75e73be7a76480be7c0a752c0b9b34e84ddb80f8
cainns98 <cainns98@users.noreply.github.com>
2018-01-28 16:41:51 -0600

    BallTracking to Class (#23)
    * Clean Up Code and Update to Python 3.6
    
    PIP 8 Compliant
    Corrected var names to be lowercase
    Corrected method names to be lowercase
    Removed unnecessary parenthesis
    Removed unneeded code
    Added Parenthesis to print statements
    
    * BallTracking to Class
    
    Reformatted BallTracking.py to function as a Python Class
    Updated a few variables for proper convention
    Removed unnecessary code
    Added explanatory comments

--- Autonoumous/BallTracking.py
+++ Autonoumous/BallTracking.py
@@ -3,203 +3,178 @@ import numpy as np
 import serial
 import _thread
 
-# Tilt up/down
-tilt = 70
-# Pan left/right conditional
-pan = 0
-myString = ""
 
-threadTwoEnd = True
-tiltMoved = False
-found = False
-pos_x = 0
-pos_y = 0
-ser = serial.Serial()
+class BallTracking:
+    def __init__(self):
+        # Tilt up/down
+        self.tilt = 70
 
+        # Pan left/right conditional
+        self.pan = 0
+        self.my_string = ""
 
-def connect():
-    global camera
+        # control variables
+        self.thread_two_end = True
+        self.tilt_moved = False
+        self.found = False
+        self.pos_x = 0
+        self.pos_y = 0
+        self.ser = serial.Serial()
 
-    # Open serial port
-    ser.baudrate = 9600
+        # start thread on pi
+        _thread.start_new_thread(self.ball_tracking, ())
 
-    # look for a serial port in use
-    for i in range(0, 10):
-        try:
-            print('../../../../../dev/ttyACM' + str(i))
-            ser.port = '../../../../../dev/ttyACM' + str(i)
-            ser.timeout = 1
-            ser.open()
-            break
-        except:
+    def connect(self):
+        # Open serial port
+        self.ser.baudrate = 9600
+
+        # look for a serial port in use
+        for i in range(0, 10):
             try:
-                print('../../../../../dev/ttyUSB' + str(i))
-                ser.port = '../../../../../dev/ttyUSB' + str(i)
-                ser.timeout = 1
-                ser.open()
+                print('../../../../../dev/ttyACM' + str(i))
+                self.ser.port = '../../../../../dev/ttyACM' + str(i)
+                self.ser.timeout = 1
+                self.ser.open()
                 break
             except:
                 try:
-                    print('COM' + str(i))
-                    ser.port = 'COM' + str(i)
-                    ser.timeout = 1
-                    ser.open()
+                    print('../../../../../dev/ttyUSB' + str(i))
+                    self.ser.port = '../../../../../dev/ttyUSB' + str(i)
+                    self.ser.timeout = 1
+                    self.ser.open()
                     break
                 except:
-                    continue
+                    try:
+                        print('COM' + str(i))
+                        self.ser.port = 'COM' + str(i)
+                        self.ser.timeout = 1
+                        self.ser.open()
+                        break
+                    except:
+                        continue
 
-
-def find_ball(image):
-    largest_contour = 0
-    cont_index = 0
-    img, contours, hierarchy = cv2.findContours(image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    for idx, contour in enumerate(contours):
-        area = cv2.contourArea(contour)
-        if area > largest_contour:
-            largest_contour = area
-            cont_index = idx
-    r = (0, 0, 2, 2)
-    if len(contours) > 0:
-        r = cv2.boundingRect(contours[cont_index])
-    return r, largest_contour
-
-
-def track():
-    global tilt
-    global tiltMoved
-    global pan
-    global pos_x
-    global pos_y
-    global found
-    if pos_x < 100 and found:
-        # pan left
-        pan = 1
-    else:
-        if pos_x > 540 and found:
-            # pan right
-            pan = 2
+    def track(self):
+        if self.pos_x < 100 and self.found:
+            # pan left
+            self.pan = 1
         else:
-            pan = 0
+            if self.pos_x > 540 and self.found:
+                # pan right
+                self.pan = 2
+            else:
+                self.pan = 0
 
-    if pos_y < 100 and found:
-        tilt = tilt + 1
-        tiltMoved = True
-    else:
-        if pos_y > 340 and found:
-            tilt = tilt - 1
-            tiltMoved = True
+        if self.pos_y < 100 and self.found:
+            self.tilt = self.tilt + 1
+            self.tilt_moved = True
         else:
-            tiltMoved = False
+            if self.pos_y > 340 and self.found:
+                self.tilt = self.tilt - 1
+                self.tilt_moved = True
+            else:
+                self.tilt_moved = False
 
+    def message(self):
+        while self.thread_two_end:
+            my_hash = self.pan + self.tilt
+            self.my_string = str(self.pan) + "," + str(self.tilt) + "," + str(my_hash)
+            self.ser.write(self.my_string + '\n')
+            print(self.my_string + '{' + self.ser.readline() + '}')
+            # 'q' to quit
+        _thread.exit()
 
-def message():
-    global myString
-    global tilt
-    global pan
-    while threadTwoEnd:
-        myHash = pan + tilt
-        myString = str(pan) + "," + str(tilt) + "," + str(myHash)
-        ser.write(myString + '\n')
-        print(myString + '{' + ser.readline() + '}')
-        # 'q' to quit
-    _thread.exit()
+        # print("no message")
 
-    # print("no message")
+    def ball_tracking(self):
+        # Keep running until 'q' is pressed
 
+        # look for a camera port in use
+        self.connect()
 
-def ball_tracking():
-    # Capture video from camera
-    global tilt
-    global found
-    global pan
-    global pos_x
-    global pos_y
-    # Create a window for the sliders and HSV image.
-    cv2.namedWindow("Original")
-    cv2.namedWindow("HSV")
-    cv2.namedWindow("Final")
+        # start communication thread
+        _thread.start_new_thread(self.message, ())
 
-    # Create the sliders
-    # ToDo: Create Trackbar Sliders
+        # capture video from camera
+        camera = cv2.VideoCapture(0)
 
-    # Keep running until 'q' is pressed
-    # look for a camera port in use
-    connect()
+        while self.thread_two_end:
+            # Get an image from the camera
+            ret, frame = camera.read()
 
-    _thread.start_new_thread(message, ())
-    camera = cv2.VideoCapture(0)
+            # Convert frame from BGR to HSV, store it in frame2
+            frame2 = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-    while threadTwoEnd:
-        # Get an image from the camera
-        ret, frame = camera.read()
+            # Get the values of all of the sliders
+            # TODO: Get Trackbar Data
+            # TEMP TRACKBAR DATA ARRAY:
+            trackbar_data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-        # Convert frame from BGR to HSV, store it in frame2
-        frame2 = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+            h_lo = trackbar_data[0]
+            h_hi = trackbar_data[1]
+            s_lo = trackbar_data[2]
+            s_hi = trackbar_data[3]
+            v_lo = trackbar_data[4]
+            v_hi = trackbar_data[5]
+            e_itr = trackbar_data[6]
+            d_itr = trackbar_data[7]
+            e_box = trackbar_data[8]
+            d_box = trackbar_data[9]
 
-        # Get the values of all of the sliders
-        # TODO: Get Trackbar Info
+            # Create arrays for the lower and upper HSV values
+            lower_hsv = np.array([h_lo, s_lo, v_lo])
+            upper_hsv = np.array([h_hi, s_hi, v_hi])
 
-        # TEMP TRACKBAR DATA ARRAY
-        trackbar_data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+            # Use the inRange function to filter the be submerged by watHSV values
+            frame3 = cv2.inRange(frame2, lower_hsv, upper_hsv)
 
-        h_lo = trackbar_data[0]
-        h_hi = trackbar_data[1]
-        s_lo = trackbar_data[2]
-        s_hi = trackbar_data[3]
-        v_lo = trackbar_data[4]
-        v_hi = trackbar_data[5]
-        e_itr = trackbar_data[6]
-        d_itr = trackbar_data[7]
-        e_box = trackbar_data[8]
-        d_box = trackbar_data[9]
+            # Morphological transformations
+            if e_itr < 1 or d_itr < 1 or e_box < 2 or d_box < 2:
+                combined_img = frame3
+            else:
+                erosion = cv2.erode(frame3, (e_box, e_box), iterations=e_itr)
+                dilation = cv2.dilate(erosion, (e_box, e_box), iterations=d_itr)
+                combined_img = dilation
 
-        # Create arrays for the lower and upper HSV values
-        lower_hsv = np.array([h_lo, s_lo, v_lo])
-        upper_hsv = np.array([h_hi, s_hi, v_hi])
+            # Locates the Ball
+            location, area = self.find_ball(combined_img)
+            x, y, w, h = location  # top_left_x, top_left_y, width, height
 
-        # Use the inRange function to filter the be submerged by watHSV values
-        frame3 = cv2.inRange(frame2, lower_hsv, upper_hsv)
+            # identify and track if large enough
+            if (w * h) < 10:
+                self.found = False
+            else:
+                self.found = True
+                center_x = x + (w / 2)
+                center_y = y + (h / 2)
+                x_pos = "X: {}".format(center_x)
+                y_pos = "Y: {}".format(center_y)
+                cv2.putText(frame, x_pos, (0, 20), cv2.FONT_HERSHEY_DUPLEX, 0.5, (255, 255, 1), 1, cv2.LINE_AA)
+                cv2.putText(frame, y_pos, (0, 40), cv2.FONT_HERSHEY_DUPLEX, 0.5, (255, 255, 1), 1, cv2.LINE_AA)
+                cv2.circle(frame, (int(center_x), int(center_y)), 3, (0, 110, 255), -1)
+                self.pos_x = center_x
+                self.pos_y = center_y
 
-        # Morphological transformations
-        if e_itr < 1 or d_itr < 1 or e_box < 2 or d_box < 2:
-            combined_img = frame3
-        else:
-            erosion = cv2.erode(frame3, (e_box, e_box), iterations=e_itr)
-            dilation = cv2.dilate(erosion, (e_box, e_box), iterations=d_itr)
-            combined_img = dilation
+            self.track()
 
-        loct, area = find_ball(combined_img)
-        x, y, w, h = loct
+            # Check if q is pressed
+            if cv2.waitKey(1) & 0xFF == ord("q"):
+                self.thread_two_end = False
 
-        if (w * h) < 10:
-            found = False
-        else:
-            found = True
-            center_x = x + (w / 2)
-            center_y = y + (h / 2)
-            x_pos = "X: {}".format(center_x)
-            y_pos = "Y: {}".format(center_y)
-            cv2.putText(frame, x_pos, (0, 20), cv2.FONT_HERSHEY_DUPLEX, 0.5, (255, 255, 1), 1, cv2.LINE_AA)
-            cv2.putText(frame, y_pos, (0, 40), cv2.FONT_HERSHEY_DUPLEX, 0.5, (255, 255, 1), 1, cv2.LINE_AA)
-            cv2.circle(frame, (int(center_x), int(center_y)), 3, (0, 110, 255), -1)
-            pos_x = center_x
-            pos_y = center_y
+                # Release the camera and close all windows.
+                camera.release()
+                cv2.destroyAllWindows()
 
-        track()
-
-        # Display the images
-        cv2.imshow("Original", frame)
-        cv2.imshow("HSV", frame2)
-
-        cv2.imshow("Final", combined_img)
-        if cv2.waitKey(1) & 0xFF == ord("q"):
-            global threadTwoEnd
-            threadTwoEnd = False
-            camera.release()
-            cv2.destroyAllWindows()
-            break
-
-
-# Release the camera and close all windows.
-
-_thread.start_new_thread(ball_tracking, ())
+    @staticmethod
+    def find_ball(image):
+        largest_contour = 0
+        cont_index = 0
+        img, contours, hierarchy = cv2.findContours(image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        for idx, contour in enumerate(contours):
+            area = cv2.contourArea(contour)
+            if area > largest_contour:
+                largest_contour = area
+                cont_index = idx
+        r = (0, 0, 2, 2)
+        if len(contours) > 0:
+            r = cv2.boundingRect(contours[cont_index])
+        return r, largest_contour
