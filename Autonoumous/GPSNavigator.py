fed683958f585d3bbd71568221f0de809bde5f2d
cainns98 <cainns98@users.noreply.github.com>
2018-01-30 18:37:45 -0600

    Specify Variables and Make use of Thread
    Altered instance variable names to be more discriptive
    Fixed init bug with new thread call.

--- Autonoumous/GPSNavigator.py
+++ Autonoumous/GPSNavigator.py
@@ -2,6 +2,7 @@ import math
 import argparse
 # things for GPS communication
 # make sure to run pip install sbp
+import _thread
 from sbp.client.drivers.network_drivers import TCPDriver
 from sbp.client import Handler, Framer
 from sbp.navigation import SBP_MSG_POS_LLH, MsgPosLLH
@@ -11,7 +12,7 @@ from sbp.client.drivers.pyserial_driver import PySerialDriver
 """ 
 This code provides the rover with autonomous GPS navigation capabilities.
 There will be two ways of inputting the destination coordinates:
-    - User input from keyboard (Prone to user error, e.g. Being of by +/-.0004 longitude would result
+    - User input from keyboard (Prone to user error, e.g. Being of by +/-.0004 target_longitude would result
         in an approximate positional error of +/-30 meters.
     - Read Lat/Lon decimal values from a file. (Will remove human error 
         (double/triple check values before running the code)
@@ -27,17 +28,17 @@ class GPSNavigator:
         # initialize variables
         self.history = [10]
         self.args = self.get_args()
-        self.rover_lat = 0.0
-        self.rover_long = 0.0
-        self.last_lat = 0.0
-        self.last_long = 0.0
-        self.longitude = 0.0
-        self.latitude = 0.0
-        self.bearing = 0.0
+        self.rover_latitude = 0.0
+        self.rover_longitude = 0.0
+        self.last_rover_latitude = 0.0
+        self.last_rover_longitude = 0.0
+        self.target_longitude = 0.0
+        self.target_latitude = 0.0
+        self.target_bearing = 0.0
         self.distance = 0.0
 
         # start navigation method
-        self.navigate()
+        _thread.start_new_thread(self.navigate(), ())
 
     def navigate(self):
         # Select Driver
@@ -63,23 +64,24 @@ class GPSNavigator:
 
                 # Shouldn't We need an elevation?.. that's msg.height
                 # int of accuracy is [h/v]_accuracy also there is n_sats
-                self.last_lat = self.rover_lat
-                self.last_long = self.rover_long
-                self.rover_lat = math.radians(msg.lat)
-                self.rover_long = math.radians(msg.lon)
+                self.last_rover_latitude = self.rover_latitude
+                self.last_rover_longitude = self.rover_longitude
+                self.rover_latitude = math.radians(msg.lat)
+                self.rover_longitude = math.radians(msg.lon)
 
-                # Get longitude, latitude, (not yet...height, and number of satellites) - need it in degrees
+                # Get target_longitude, target_latitude,
+                # (not yet...height, and number of satellites) - need it in degrees
                 if self.args.bearing is None and self.args.distance is None:
-                    self.longitude = math.radians(float(self.args.longitude))
-                    self.latitude = math.radians(float(self.args.latitude))
+                    self.target_longitude = math.radians(float(self.args.longitude))
+                    self.target_latitude = math.radians(float(self.args.latitude))
 
-                # If bearing and distance were supplied, get latitude and longitude in degrees
+                # If target_bearing and distance were supplied, get target_latitude and target_longitude in degrees
                 else:
-                    self.bearing = float(self.args.bearing)
+                    self.target_bearing = float(self.args.bearing)
                     self.distance = float(self.args.distance)
-                    self.latitude, self.longitude = self.get_gps_coordinates()
-                    self.latitude = math.radians(self.latitude)
-                    self.longitude = math.radians(self.longitude)
+                    self.target_latitude, self.target_longitude = self.get_gps_coordinates()
+                    self.target_latitude = math.radians(self.target_latitude)
+                    self.target_longitude = math.radians(self.target_longitude)
 
                 self.distance = self.get_distance()
                 theta = self.get_target_bearing()
@@ -97,8 +99,8 @@ class GPSNavigator:
                 # Calculate rover_heading using the atan2 function
                 rover_heading = math.atan2(vel__east, vel__north)
 
-                # TODO: turn(bearing) # something like: if math.abs(bearing) > tolerance then turn else drive (distance)
-                # TODO: drive(distance)
+                # TODO: turn(target_bearing) # something like: if math.abs(target_bearing) > tolerance
+                # TODO: then turn else drive (distance) drive(distance)
 
                 # Need to fix this to output the proper value of theta
                 print("Bearing: ", math.degrees(theta), ", Distance: ", self.distance,
@@ -110,18 +112,18 @@ class GPSNavigator:
                     # TODO: put a break to say we are finished
 
     def get_gps_coordinates(self):
-        # Convert the rover latitude and longitude to radians
-        lat1 = math.radians(self.rover_lat)
-        lon1 = math.radians(self.rover_long)
-        bearing = math.radians(self.bearing)
+        # Convert the rover target_latitude and target_longitude to radians
+        lat1 = math.radians(self.rover_latitude)
+        lon1 = math.radians(self.rover_longitude)
+        bearing = math.radians(self.target_bearing)
 
-        # Takes the lat1, lon2, distance, and bearing does the math needed
+        # Takes the lat1, lon2, distance, and target_bearing does the math needed
         lat2 = math.asin(math.sin(lat1) * math.cos(self.distance / self.RADIUS_OF_EARTH) +
                          math.cos(lat1) * math.sin(self.distance / self.RADIUS_OF_EARTH) * math.cos(bearing))
         lon2 = lon1 + math.atan2(math.sin(bearing) * math.sin(self.distance / self.RADIUS_OF_EARTH) * math.cos(lat1),
                                  math.cos(self.distance / self.RADIUS_OF_EARTH) - math.sin(lat1) * math.sin(lat2))
 
-        # Change the latitude and longitude to degrees
+        # Change the target_latitude and target_longitude to degrees
         lat2 = math.degrees(lat2)
         lon2 = math.degrees(lon2)
         return lat2, lon2
@@ -130,38 +132,39 @@ class GPSNavigator:
         # Haversine Formula:
         # a = sin((lat2-lat1)/2) * sin((lat2-lat1)/2) + cos(lat1) * cos(lat2) * sin((lon2-lon1)/2) * sin((lon2-lon1)/2)
         # Calculate change in Latitude
-        delta_lat = self.latitude - self.rover_lat
+        delta_lat = self.target_latitude - self.rover_latitude
         # Calculate change in Longitude
-        delta_lon = self.longitude - self.rover_long
+        delta_lon = self.target_longitude - self.rover_longitude
         # Haversine formula for distance
         a = math.sin(delta_lat / 2) * math.sin(delta_lat / 2) + \
-            math.cos(self.rover_lat) * math.cos(self.latitude) * math.sin(delta_lon / 2) * math.sin(delta_lon / 2)
+            math.cos(self.rover_latitude) * math.cos(self.target_latitude) * math.sin(delta_lon / 2) * \
+            math.sin(delta_lon / 2)
         c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
         d = self.RADIUS_OF_EARTH * c
         return d
 
     def get_target_bearing(self):
         # Calculate change in Latitude
-        # delta_lat = latitude - rover_lat
+        # delta_lat = target_latitude - rover_latitude
         # Calculate change in Longitude
-        delta_lon = self.longitude - self.rover_long
+        delta_lon = self.target_longitude - self.rover_longitude
         # Bearing, represented as nearest offset from North. (+/-180 degrees from North)
-        theta = math.atan2(math.sin(delta_lon) * math.cos(self.latitude),
-                           math.cos(self.rover_lat) * math.sin(self.latitude) -
-                           math.sin(self.rover_lat) * math.cos(self.latitude) * math.cos(delta_lon))
+        theta = math.atan2(math.sin(delta_lon) * math.cos(self.target_latitude),
+                           math.cos(self.rover_latitude) * math.sin(self.target_latitude) -
+                           math.sin(self.rover_latitude) * math.cos(self.target_latitude) * math.cos(delta_lon))
         return theta
 
     def get_rover_bearing(self):
         # FIX THIS:
-        # return math.atan2((rover_lat - last_long)/( roverLong - last_long))
+        # return math.atan2((rover_latitude - last_rover_longitude)/( roverLong - last_rover_longitude))
         # Calculate change in Latitude
-        # deltaLat = rover_lat - last_lat
+        # deltaLat = rover_latitude - last_rover_latitude
         # Calculate change in Longitude
-        delta_lon = self.rover_long - self.last_long
+        delta_lon = self.rover_longitude - self.last_rover_longitude
         # Bearing, represented as nearest offset from North. (+/-180 degrees from North)
-        theta = math.atan2(math.sin(delta_lon) * math.cos(self.rover_lat),
-                           math.cos(self.last_lat) * math.sin(self.rover_lat) -
-                           math.sin(self.last_lat) * math.cos(self.rover_lat) * math.cos(delta_lon))
+        theta = math.atan2(math.sin(delta_lon) * math.cos(self.rover_latitude),
+                           math.cos(self.last_rover_latitude) * math.sin(self.rover_latitude) -
+                           math.sin(self.last_rover_latitude) * math.cos(self.rover_latitude) * math.cos(delta_lon))
         return theta
 
     @staticmethod
@@ -170,18 +173,18 @@ class GPSNavigator:
         Get and parse arguments.
         """
         parser = argparse.ArgumentParser(
-            description="Travel to a specified latitude and longitude")
+            description="Travel to a specified target_latitude and target_longitude")
         parser.add_argument(
-            "--longitude",
+            "--target_longitude",
             default=-122.41883,
-            help="specify the longitude in decimal.")
+            help="specify the target_longitude in decimal.")
         parser.add_argument(
-            "--latitude",
+            "--target_latitude",
             default=37.77432,
-            help="specify the latitude in decimal.")
+            help="specify the target_latitude in decimal.")
         parser.add_argument(
-            "--bearing",
-            help="specify the bearing to the destination in degrees.")
+            "--target_bearing",
+            help="specify the target_bearing to the destination in degrees.")
         parser.add_argument(
             "--distance",
             help="specify the distance to the destination in meters.")
