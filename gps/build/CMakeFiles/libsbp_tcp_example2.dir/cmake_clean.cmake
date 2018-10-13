199fbdf0593a0282a7787ab10c9067c373a62afe
Tom-bomb <thomas.c.buckman-1@ou.edu>
2018-10-13 10:00:54 -0500

    GPS code Added
    Folder "GPS" added. The working example is called "test" in this GPS directory, and you can see what files I used in the Makefile. Most of these are part of the SBP library that I cloned, but I wrote gps.c and gps.h, so if you get bugs that's where they'll be.

---
+++ gps/build/CMakeFiles/libsbp_tcp_example2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/libsbp_tcp_example2.dir/tcp_example2.c.o"
+  "libsbp_tcp_example2.pdb"
+  "libsbp_tcp_example2"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/libsbp_tcp_example2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
